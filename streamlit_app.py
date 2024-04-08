a4c7718868ab7a0ae5e657a7388cf8348ef8b035
wescogan <61040946+wescogan@users.noreply.github.com>
2024-04-08 16:26:50 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,39 +23,15 @@ st.stop()
 
 color_or_style = st.selectbox(
     'Pick a sweatsuit color or style'
-    , my_dataframe
+    , pd_df
 )
 
 caption = 'Our warm, ' + color_or_style + ' sweatsuit!'
 
 if color_or_style:
-    st.image(
+    row = pd_df.loc[pd_df['color_or_style'] == color_or_style].iloc[0]
+    st.write(row);
+    #st.image(row.)
     #st.write(ingredients_list)
     #st.text(ingredients_list)
-
-    ingredients_string = ''
-
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        
-        st.subheader(fruit_chosen + ' Nutritional Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        fv_dv = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-    
-    st.write(ingredients_string)
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-                values ('""" + ingredients_string + """','""" + name_on_order + """ ')"""
-
-    #st.write(my_insert_stmt)
-    #st.stop()
-    time_to_insert = st.button('Submit order')
-
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        
-        st.success('Your Smoothie is ordered, ' + name_on_order +  '!', icon="✅")
         

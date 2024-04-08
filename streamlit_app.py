4b74ffd1ba1a0a2b269b30e500546fc816d41d63
wescogan <61040946+wescogan@users.noreply.github.com>
2024-04-08 16:33:07 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,11 @@ from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title("Zena's Amazing Athleisure Catalog")
 
-name_on_order = st.text_input('Pick a sweatsuit color or style:')
-st.write('The name on your smoothie will be:', name_on_order)
-
 conn = st.connection("snowflake")
 session = conn.session()
 table = session.table("ZENAS_ATHLEISURE_DB.PRODUCTS.catalog_for_website")
 my_dataframe = table# .select(col('color_or_style'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
 # Convert Snowpark Dataframe to Pandas Dataframe so we can use LOC function
@@ -23,7 +20,7 @@ st.dataframe(pd_df)
 
 color_or_style = st.selectbox(
     'Pick a sweatsuit color or style'
-    , pd_df
+    , my_dataframe.select(col('color_or_style'))
 )
 
 caption = 'Our warm, ' + color_or_style + ' sweatsuit!'
